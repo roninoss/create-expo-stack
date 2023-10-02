f9630457077519edafcde83a82241adb4157df6d
Dan Stepanov <dan@danstepanov.com>
2023-10-01 21:33:16 -0700

    run tests for all package managers

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -14,146 +14,146 @@ const packageManagers = [`npm`, `yarn`, `pnpm`]
 
 test(`outputs version`, async () => {
   const output = await cli(`--version`)
-  expect(output).toContain(`2.0.6`)
+  expect(output).toContain(`2.0.8`)
 })
 
 test(`outputs help`, async () => {
   const output = await cli(`--help`)
-  expect(output).toContain(`2.0.6`)
+  expect(output).toContain(`2.0.8`)
 })
 
 for (const packageManager of packageManagers) {
   // --default
   test(`generates a default project with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --default --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --react-navigation stack
   test(`generates a project with react-navigation stack with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation stack and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation stack and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --react-navigation stack nativewind
   test(`generates a project with react-navigation stack and nativewind with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --nativewind --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation stack and nativewind and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --nativewind --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation stack and nativewind and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --stack --nativewind --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --react-navigation tabs
   test(`generates a project with react-navigation tabs with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation tabs and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation tabs and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --react-navigation tabs nativewind
   test(`generates a project with react-navigation tabs and nativewind with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --nativewind --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation tabs and nativewind and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --nativewind --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with react-navigation tabs and nativewind and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --react-navigation --tabs --nativewind --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --expo-router stack
   test(`generates a project with expo-router stack with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router stack and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router stack and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --expo-router stack nativewind
   test(`generates a project with expo-router stack and nativewind with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --nativewind --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router stack and nativewind and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --nativewind --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router stack and nativewind and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --stack --nativewind --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --expo-router tabs
   test(`generates a project with expo-router tabs with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router tabs and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router tabs and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   // --expo-router tabs nativewind
   test(`generates a project with expo-router tabs and nativewind with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --nativewind --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router tabs and nativewind and noGit with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --nativewind --noGit --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 
   test(`generates a project with expo-router tabs and nativewind and noGit and noInstall with ${packageManager}`, async () => {
     const output = await cli(`myTestProject --expo-router --tabs --nativewind --noGit --noInstall --nonInteractive --${packageManager}`)
-    expect(output).toContain(`Success!`)
+    expect(output).toContain(packageManager)
   })
 }
