c874a8dbdd9c8a065a8a95579324a37768b53d89
Dan Stepanov <dan@danstepanov.com>
2023-10-08 02:55:36 -0700

    bump version number v2.0.19

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -14,12 +14,12 @@ const packageManagers = [`npm`, `yarn`, `pnpm`]
 
 test(`outputs version`, async () => {
   const output = await cli(`--version`)
-  expect(output).toContain(`2.0.18`)
+  expect(output).toContain(`2.0.19`)
 })
 
 test(`outputs help`, async () => {
   const output = await cli(`--help`)
-  expect(output).toContain(`2.0.18`)
+  expect(output).toContain(`Info`)
 })
 
 for (const packageManager of packageManagers) {
