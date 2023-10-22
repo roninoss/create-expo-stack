0b6317beba6c85295ae56de1172358f78f9e8b13
unknown <agrit1408@gmail.com>
2023-10-22 14:18:51 +0530

    Setting up Prompts for release workflow
    Working on setting up EAS for the first iteration, then will work on codepush

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,35 @@
 // export types
 export interface CliFlags {
-  noGit: boolean;
-  noInstall: boolean;
-  importAlias: string;
+  noGit: boolean
+  noInstall: boolean
+  importAlias: string
 }
 
-export const availablePackages = ["nativewind", "tamagui", "react-navigation", "expo-router", "stylesheet"] as const;
+export const availablePackages = [
+  'nativewind',
+  'tamagui',
+  'react-navigation',
+  'expo-router',
+  'stylesheet',
+] as const
 
-export type NavigationTypes = "stack" | "tabs" | {};
+export type NavigationTypes = 'stack' | 'tabs' | {}
 
 export type AvailablePackages = {
-  name: (typeof availablePackages)[number];
-  type: "navigation" | "styling";
-  options: NavigationTypes;
+  name: (typeof availablePackages)[number]
+  type: 'navigation' | 'styling' | 'releaseWorkflow'
+  options: NavigationTypes
 }
 
 export interface CliResults {
-  projectName: string;
-  packages: AvailablePackages[];
-  flags: CliFlags;
+  projectName: string
+  packages: AvailablePackages[]
+  flags: CliFlags
+}
+
+export type CliFeatureType = {
+  type: string
+  name: string
+  message: string
+  choices: Array<string>
 }
--- src/utilities/runCLI.ts
+++ src/utilities/runCLI.ts
@@ -1,21 +1,21 @@
-import { Toolbox } from "gluegun/build/types/domain/toolbox";
+import { Toolbox } from 'gluegun/build/types/domain/toolbox'
 
-import { DEFAULT_APP_NAME, defaultOptions } from "../constants";
-import { CliResults } from "../types";
+import { DEFAULT_APP_NAME, defaultOptions } from '../constants'
+import { CliFeatureType, CliResults } from '../types'
 
 export async function runCLI(toolbox: Toolbox): Promise<CliResults> {
   const {
     parameters: { first },
-    print: { success },
-    prompt: { ask, confirm }
-  } = toolbox;
+    print: { success, error },
+    prompt: { ask, confirm },
+  } = toolbox
 
   // Set the default options
-  let cliResults = defaultOptions;
+  let cliResults = defaultOptions
 
   // Set the app name if it was passed in via the initial command
   if (first) {
-    cliResults.projectName = first;
+    cliResults.projectName = first
   } else {
     const askName = {
       type: 'input',
@@ -23,11 +23,11 @@ export async function runCLI(toolbox: Toolbox): Promise<CliResults> {
       message: `What do you want to name your project? (${DEFAULT_APP_NAME})`,
     }
     const { name } = await ask(askName)
-    cliResults.projectName = name || DEFAULT_APP_NAME;
+    cliResults.projectName = name || DEFAULT_APP_NAME
   }
 
   // Clear default packages
-  cliResults.packages = [];
+  cliResults.packages = []
   // Ask about TypeScript
   const useTypescript = await confirm(
     'Would you like to use TypeScript with this project?',
@@ -41,14 +41,14 @@ export async function runCLI(toolbox: Toolbox): Promise<CliResults> {
   }
 
   // Ask about navigation
-  const askNavigation = {
+  const askNavigation: CliFeatureType = {
     type: 'select',
     name: 'navigationSelect',
     message: 'What would you like to use for Navigation?',
     choices: ['React Navigation', 'Expo Router', 'None'],
   }
 
-  const askNavigationType = {
+  const askNavigationType: CliFeatureType = {
     type: 'select',
     name: 'navigationTypeSelect',
     message: 'What type of navigation would you like to use?',
@@ -58,36 +58,82 @@ export async function runCLI(toolbox: Toolbox): Promise<CliResults> {
   const { navigationSelect } = await ask(askNavigation)
 
   if (navigationSelect !== 'None') {
-    const { navigationTypeSelect } = await ask(askNavigationType);
+    const { navigationTypeSelect } = await ask(askNavigationType)
     if (navigationSelect === 'React Navigation') {
-      cliResults.packages.push({ name: "react-navigation", type: 'navigation', options: navigationTypeSelect.toLowerCase() });
+      cliResults.packages.push({
+        name: 'react-navigation',
+        type: 'navigation',
+        options: navigationTypeSelect.toLowerCase(),
+      })
     } else {
-      cliResults.packages.push({ name: "expo-router", type: 'navigation', options: navigationTypeSelect.toLowerCase() });
+      cliResults.packages.push({
+        name: 'expo-router',
+        type: 'navigation',
+        options: navigationTypeSelect.toLowerCase(),
+      })
     }
     success(`Great, we'll use ${navigationSelect}!`)
   } else {
     success(`No problem, skipping navigation for now.`)
   }
 
-  const askStyling = {
-    type: "select",
-    name: "stylingSelect",
-    message: "What would you like to use for styling?",
-    choices: ['tamagui', 'nativewind', 'stylesheet']
+  const askStyling: CliFeatureType = {
+    type: 'select',
+    name: 'stylingSelect',
+    message: 'What would you like to use for styling?',
+    choices: ['tamagui', 'nativewind', 'stylesheet'],
   }
 
   const { stylingSelect } = await ask(askStyling)
 
-
   if (stylingSelect === 'nativewind') {
-    cliResults.packages.push({ name: "nativewind", type: 'styling', options: {} });
+    cliResults.packages.push({
+      name: 'nativewind',
+      type: 'styling',
+      options: {},
+    })
     success(`You'll be styling with ease using Tailwind.`)
   } else if (stylingSelect === 'tamagui') {
-    cliResults.packages.push({ name: "tamagui", type: 'styling', options: {} });
+    cliResults.packages.push({ name: 'tamagui', type: 'styling', options: {} })
+    success(`You'll be set with using cross-platform styling`)
   } else {
-    cliResults.packages.push({ name: "stylesheet", type: 'styling', options: {} });
-    success(`Cool, you're using the default StyleSheet that comes with React Native.`)
+    cliResults.packages.push({
+      name: 'stylesheet',
+      type: 'styling',
+      options: {},
+    })
+    success(
+      `Cool, you're using the default StyleSheet that comes with React Native.`
+    )
   }
 
-  return cliResults;
+  // Ask about release workflow setup
+
+  const askReleaseWorkflowSetup: CliFeatureType = {
+    type: 'select',
+    name: 'releaseWorkflowSetup',
+    message: 'How would you like to setup your release workflow?',
+    choices: ['EAS', 'Codepush', 'none'],
+  }
+
+  const isEAScliInstalled = await confirm(
+    'Do you have EAS cli installed ? ',
+    true
+  )
+
+  const { releaseWorkflowSetup } = await ask(askReleaseWorkflowSetup)
+
+  if (releaseWorkflowSetup === 'none') {
+    success(`Ohkay!!, so you will build apks manually with gradle and xCode.`)
+  } else if (releaseWorkflowSetup === 'EAS') {
+    isEAScliInstalled
+      ? success(
+          ` Great! EAS will set your environments, namely development, staging and production , and you can share your app and send OTA updates to any specific environment`
+        )
+      : error(`Need to install EAS cli to setup EAS release workflow`)
+  } else if (releaseWorkflowSetup === 'Codepush') {
+    success(`Great you chose to setup Codepush updates`)
+  }
+
+  return cliResults
 }
