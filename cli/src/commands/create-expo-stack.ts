83b07372a735123a88997510e98c954217d67a01
Dan Stepanov <dan@danstepanov.com>
2023-11-07 20:38:33 -0800

    tamagui tab navigation examples working, not perfect but working

--- cli/src/commands/create-expo-stack.ts
+++ cli/src/commands/create-expo-stack.ts
@@ -2,181 +2,178 @@ import { GluegunCommand } from 'gluegun'
 const util = require('util')
 
 import {
-  configureProjectFiles,
-  generateProjectFiles,
-  printOutput,
-  renderTitle,
-  runCLI,
-  runIgnite,
-  showHelp,
+	configureProjectFiles,
+	generateProjectFiles,
+	printOutput,
+	renderTitle,
+	runCLI,
+	runIgnite,
+	showHelp,
 } from '../utilities'
 import { defaultOptions } from '../constants'
 import { CliResults, availablePackages } from '../types'
 
 const command: GluegunCommand = {
-  name: 'create-expo-stack',
-  description: 'Create a new Expo project',
-  run: async (toolbox) => {
-    const {
-      parameters: { first, options },
-      print: { info, highlight, warning },
-    } = toolbox
-    if (options.help || options.h) {
-      showHelp(info, highlight, warning)
+	name: 'create-expo-stack',
+	description: 'Create a new Expo project',
+	run: async (toolbox) => {
+		const {
+			parameters: { first, options },
+			print: { info, highlight, warning },
+		} = toolbox
+		if (options.help || options.h) {
+			showHelp(info, highlight, warning)
 
-      return
-    }
-    try {
-      // Validation: check if the user passed in the tabs option without passing in either expo router or react navigation. If so, throw an error
-      if (
-        options.tabs &&
-        !options.reactNavigation &&
-        !options['react-navigation'] &&
-        !options.reactnavigation &&
-        !options.expoRouter &&
-        !options['expo-router'] &&
-        !options.exporouter
-      ) {
-        throw new Error(
-          'You must pass in either --react-navigation or --expo-router if you want to use the --tabs option',
-        )
-      }
+			return
+		}
+		try {
+			// Validation: check if the user passed in the tabs option without passing in either expo router or react navigation. If so, throw an error
+			if (
+				options.tabs &&
+				!options.reactNavigation &&
+				!options['react-navigation'] &&
+				!options.reactnavigation &&
+				!options.expoRouter &&
+				!options['expo-router'] &&
+				!options.exporouter
+			) {
+				throw new Error(
+					'You must pass in either --react-navigation or --expo-router if you want to use the --tabs option',
+				)
+			}
 
-      await renderTitle(toolbox)
+			await renderTitle(toolbox)
 
-      // TODO: this is hacky, figure out a way to do this better
-      // set timeout for 1 second so that the title can render before the CLI runs
-      await new Promise((resolve) => setTimeout(resolve, 200))
+			// TODO: this is hacky, figure out a way to do this better
+			// set timeout for 1 second so that the title can render before the CLI runs
+			await new Promise((resolve) => setTimeout(resolve, 200))
 
-      // Set the default options
-      let cliResults: CliResults = defaultOptions
+			// Set the default options
+			let cliResults: CliResults = defaultOptions
 
-      // Check if user wants to create an opinionated stack prior to running the configurable CLI
-      if (options.ignite) {
-        await runIgnite(toolbox)
-      } else {
-        // Conditionally skip running the CLI
-        const useDefault =
-          (options.default !== undefined && options.default) ||
-          (options.d !== undefined && options.d)
-        const skipCLI = options.nonInteractive
-        const useBlankTypescript = options.blank || false
-        // Check if any of the options were passed in via the command
-        const optionsPassedIn = availablePackages.some(
-          (condition) => options[condition] !== undefined,
-        )
+			// Check if user wants to create an opinionated stack prior to running the configurable CLI
+			if (options.ignite) {
+				await runIgnite(toolbox)
+			} else {
+				// Conditionally skip running the CLI
+				const useDefault =
+					(options.default !== undefined && options.default) ||
+					(options.d !== undefined && options.d)
+				const skipCLI = options.nonInteractive
+				const useBlankTypescript = options.blank || false
+				// Check if any of the options were passed in via the command
+				const optionsPassedIn = availablePackages.some(
+					(condition) => options[condition] !== undefined,
+				)
 
-        if (!(useDefault || optionsPassedIn || skipCLI || useBlankTypescript)) {
-          //  Run the CLI to prompt the user for input
-          cliResults = await runCLI(toolbox)
-        }
+				if (!(useDefault || optionsPassedIn || skipCLI || useBlankTypescript)) {
+					//  Run the CLI to prompt the user for input
+					cliResults = await runCLI(toolbox)
+				}
 
-        // @internal Update the cliResults with the options passed in via the command
-        // This is used for testing purposes only
-        if (
-          options.reactNavigation ||
-          options['react-navigation'] ||
-          options.reactnavigation
-        ) {
-          // Add react-navigation package
-          cliResults.packages.push({
-            name: 'react-navigation',
-            type: 'navigation',
-            options: {
-              type: options.tabs ? 'tabs' : 'stack',
-            },
-          })
-        }
-        if (
-          options.expoRouter ||
-          options['expo-router'] ||
-          options.exporouter
-        ) {
-          // Add expo-router package
-          cliResults.packages.push({
-            name: 'expo-router',
-            type: 'navigation',
-            options: {
-              type: options.tabs ? 'tabs' : 'stack',
-            },
-          })
-        }
-        if (options.nativewind) {
-          // Add nativewind package
-          cliResults.packages.push({
-            name: 'nativewind',
-            type: 'styling',
-            options: {},
-          })
-        }
-        if (options.tamagui) {
-          // Add tamagui package
-          cliResults.packages.push({
-            name: 'tamagui',
-            type: 'styling',
-            options: {},
-          })
-        }
-        if (options.stylesheet || options.stylesheets) {
-          // Add stylesheet package
-          cliResults.packages.push({
-            name: 'stylesheet',
-            type: 'styling',
-            options: {},
-          })
-        }
+				// @internal Update the cliResults with the options passed in via the command
+				// This is used for testing purposes only
+				if (
+					options.reactNavigation ||
+					options['react-navigation'] ||
+					options.reactnavigation
+				) {
+					// Add react-navigation package
+					cliResults.packages.push({
+						name: 'react-navigation',
+						type: 'navigation',
+						options: {
+							type: options.tabs ? 'tabs' : 'stack',
+						},
+					})
+				}
+				if (
+					options.expoRouter ||
+					options['expo-router'] ||
+					options.exporouter
+				) {
+					// Add expo-router package
+					cliResults.packages.push({
+						name: 'expo-router',
+						type: 'navigation',
+						options: {
+							type: options.tabs ? 'tabs' : 'stack',
+						},
+					})
+				}
+				if (options.nativewind) {
+					// Add nativewind package
+					cliResults.packages.push({
+						name: 'nativewind',
+						type: 'styling',
+					})
+				} else if (options.tamagui) {
+					// Add tamagui package
+					cliResults.packages.push({
+						name: 'tamagui',
+						type: 'styling',
+					})
+				} else {
+					// Add stylesheet package
+					cliResults.packages.push({
+						name: 'stylesheet',
+						type: 'styling',
+					})
+				}
 
-        // Destructure the results but set the projectName if the first param is passed in
-        if (first) {
-          cliResults.projectName = first
-        }
+				// Destructure the results but set the projectName if the first param is passed in
+				if (first) {
+					cliResults.projectName = first
+				}
 
-        // By this point, all cliResults should be set
+				// By this point, all cliResults should be set
 
-        info('Your project configuration:')
-        info(util.inspect(cliResults, false, null, true /* enable colors */))
-        const { packages } = cliResults
+				info('Your project configuration:')
+				info(util.inspect(cliResults, false, null, true /* enable colors */))
+				const { packages } = cliResults
 
-        // Define props to be passed into the templates
+				// Define props to be passed into the templates
 
-        const navigationPackage =
-          packages.find((p) => p.type === 'navigation') || undefined
-        const stylingPackage = packages.find((p) => p.type === 'styling')
+				const navigationPackage =
+					packages.find((p) => p.type === 'navigation') || undefined
+				//   if there is no styling package, add the stylesheet package
+				const stylingPackage = packages.find((p) => p.type === 'styling');
 
-        let files: string[] = []
 
-        files = configureProjectFiles(
-          files,
-          navigationPackage,
-          stylingPackage,
-          toolbox,
-        )
+				let files: string[] = []
 
-        // Once all the files are defined, format and generate them
-        let formattedFiles: any[] = []
+				files = configureProjectFiles(
+					files,
+					navigationPackage,
+					stylingPackage,
+					toolbox,
+				)
 
-        formattedFiles = generateProjectFiles(
-          cliResults,
-          files,
-          formattedFiles,
-          navigationPackage,
-          toolbox,
-          stylingPackage,
-        )
+				// Once all the files are defined, format and generate them
+				let formattedFiles: any[] = []
 
-        await printOutput(cliResults, formattedFiles, toolbox)
-      }
-    } catch (error) {
-      // TODO: delete all files
+				formattedFiles = generateProjectFiles(
+					cliResults,
+					files,
+					formattedFiles,
+					navigationPackage,
+					toolbox,
+					stylingPackage,
+				)
 
-      info(`Oops, something went wrong while creating your project 😢`)
-      info('')
-      info(`Error: ${error}`)
-      info(
-        `\nIf this was unexpected, please open an issue: https://github.com/danstepanov/create-expo-stack#reporting-bugs--feedback`,
-      )
-    }
-  },
+				await printOutput(cliResults, formattedFiles, toolbox)
+			}
+		} catch (error) {
+			// TODO: delete all files
+
+			info(`Oops, something went wrong while creating your project 😢`)
+			info('')
+			info(`Error: ${error}`)
+			info(
+				`\nIf this was unexpected, please open an issue: https://github.com/danstepanov/create-expo-stack#reporting-bugs--feedback`,
+			)
+		}
+	},
 }
 
 export default command
--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -5,11 +5,11 @@ export const TITLE_TEXT = 'create expo stack'
 export const DEFAULT_APP_NAME = 'my-expo-app'
 
 export const defaultOptions: CliResults = {
-  projectName: DEFAULT_APP_NAME,
-  packages: [],
-  flags: {
-    noGit: false,
-    noInstall: false,
-    importAlias: '~/',
-  },
+	projectName: DEFAULT_APP_NAME,
+	packages: [],
+	flags: {
+		noGit: false,
+		noInstall: false,
+		importAlias: '~/',
+	},
 }
