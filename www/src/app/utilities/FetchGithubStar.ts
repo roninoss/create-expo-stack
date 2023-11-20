e8fb3cf04209423fb6af6f03ef149da0f2414406
Sahad <msahad124@gmail.com>
2023-11-20 13:08:38 +0530

    update

--- www/src/app/utilities/FetchGithubStar.ts
+++ www/src/app/utilities/FetchGithubStar.ts
@@ -1,31 +1,31 @@
 'use client';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export function useFormattedStars() {
-  const [formattedStars, setFormattedStars] = useState(null);
+	const [formattedStars, setFormattedStars] = useState(null);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const result = await fetch('https://api.github.com/repos/danstepanov/create-expo-stack')
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-          })
-          .then((data) => {
-            return data.stargazers_count;
-          });
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const result = await fetch('https://api.github.com/repos/danstepanov/create-expo-stack')
+					.then((response) => {
+						if (!response.ok) {
+							throw new Error(`HTTP error! Status: ${response.status}`);
+						}
+						return response.json();
+					})
+					.then((data) => {
+						return data.stargazers_count;
+					});
 
-        setFormattedStars(result);
-      } catch (error) {
-        throw new Error('Fetching github star count error');
-      }
-    }
+				setFormattedStars(result);
+			} catch (error) {
+				throw new Error('Fetching github star count error');
+			}
+		}
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  return formattedStars;
+	return formattedStars;
 }
