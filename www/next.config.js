026b97ee02f370ee1ee10dad9877b540a3c623d7
Dan Stepanov <dan@danstepanov.com>
2023-11-20 02:22:29 -0500

    symlink README, add shortlink for discord, update contributors, add header labels and descriptor to README, fix styling package issues

--- www/next.config.js
+++ www/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
 	images: {
 		domains: ['avatars.githubusercontent.com']
+	},
+	async redirects() {
+		return [
+			{
+				source: '/discord',
+				destination: 'https://discord.gg/2EnARBCR',
+				permanent: true
+			}
+		]
 	}
 };
 
