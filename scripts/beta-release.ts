4fe4ac0ec96754751c4ec44e4e0d1f593ec45fd6
Ernesto Resende <dev@ernestoresende.com>
2023-11-21 21:37:12 -0300

    skip beta release when no changesets are present (#62)

--- scripts/beta-release.ts
+++ scripts/beta-release.ts
@@ -14,18 +14,21 @@ async function createBetaRelease() {
 		 */
 		const packageJson = await Bun.file(process.cwd() + '/cli/package.json').json();
 		const newBetaVersion = releasePlan.releases.find((release) => release.name === packageJson.name)?.newVersion;
-		packageJson.version = `${newBetaVersion}-beta.${commitHash}`;
 
-		/**
-		 * Write the new beta version to `package.json`. It is an ephemeral change
-		 * that lives only for the duration of the CI run, and is not committed
-		 */
-		const content = JSON.stringify(packageJson, null, '\t') + '\n';
-		await Bun.write(process.cwd() + '/cli/package.json', content);
+		if (newBetaVersion) {
+			/**
+			 * Write the new beta version to `package.json`. It is an ephemeral change
+			 * that lives only for the duration of the CI run, and is not committed
+			 */
+			packageJson.version = `${newBetaVersion}-beta.${commitHash}`;
+
+			const content = JSON.stringify(packageJson, null, '\t') + '\n';
+			await Bun.write(process.cwd() + '/cli/package.json', content);
+		}
 	} catch (error) {
 		console.log(error);
 		process.exit(1);
 	}
 }
 
-createBetaRelease();
+await createBetaRelease();
