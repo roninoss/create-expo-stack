27e6dfd188c1527f0ecc6aa9c18311b881c8b169
Ernesto Resende <dev@ernestoresende.com>
2023-11-22 00:03:36 -0300

    change vercel adapter output to `static`

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	site: `https://expostack.dev/`,
 	integrations: [react(), tailwind()],
-	output: 'server',
+	output: 'static',
 	adapter: vercel({
 		webAnalytics: {
 			enabled: true
