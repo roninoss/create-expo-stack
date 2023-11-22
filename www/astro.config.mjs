a7da1231d002d785f2e86f37d38dcf341e9397c2
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-22 13:01:26 -0800

    Added discord redirect

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,16 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/static';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-	site: `https://expostack.dev/`,
-	integrations: [react(), tailwind()],
-	output: 'static',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		}
-	})
+  site: `https://expostack.dev/`,
+  integrations: [react(), tailwind()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  redirects: {
+    "/discord": {
+      status: 302,
+      destination: "/discord",
+    },
+  },
 });
