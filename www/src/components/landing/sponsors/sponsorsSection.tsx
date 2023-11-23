80ce295e6db89834fc789f99b7baf952088b1779
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-23 13:58:44 -0800

    Removed unused code

--- www/src/components/landing/sponsors/sponsorsSection.tsx
+++ www/src/components/landing/sponsors/sponsorsSection.tsx
@@ -7,9 +7,7 @@ export default function SponsorsSection() {
       <h1 className="text-center text-4xl font-bold tracking-tight text-white md:text-6xl lg:text-[4rem] xl:text-[4rem] pb-6 pt-24 2xl:pt-16">
         Sponsors
       </h1>
-      <div className="flex flex-col items-center gap-6">
-        {/* honeycomb grid for the future */}
-        {/* [&>*:nth-child(9n_+_1)]:ml-[1.875rem] sm:[&>*:nth-child(9n_+_1)]:ml-[2.375rem] */}
+      <div className="flex flex-col items-center gap-6"> 
         <div className="flex flex-wrap gap-6 w-[18rem]items-center justify-center sm:w-[23rem]">
           {SPONSORS.map((sponsor) => (
             <Sponsor sponsor={sponsor} key={SPONSORS.indexOf(sponsor)} />
