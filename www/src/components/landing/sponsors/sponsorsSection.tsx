f578b0c705796503e45430036e3fd93a585ff6da
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-11-23 15:08:44 -0800

    Updated README sponsor icons to be round. And added sentence and sponsor hyperlink in sponsors section

--- www/src/components/landing/sponsors/sponsorsSection.tsx
+++ www/src/components/landing/sponsors/sponsorsSection.tsx
@@ -7,7 +7,19 @@ export default function SponsorsSection() {
       <h1 className="text-center text-4xl font-bold tracking-tight text-white md:text-6xl lg:text-[4rem] xl:text-[4rem] pb-6 pt-24 2xl:pt-16">
         Sponsors
       </h1>
-      <div className="flex flex-col items-center gap-6"> 
+      <div className="w-full flex items-center justify-center">
+        <div className="text-white items-center justify-center flex flex-wrap mb-6 max-w-[50ch] mx-auto text-center">
+          <span className="text-balance">
+            Support this project by becoming a sponsor. Your logo will show up
+            here with a link to your website.{" "}
+            <a href="https://github.com/sponsors/danstepanov" target="_blank" className="underline underline-offset-2 hover:text-white/80">
+              Become a sponsor.
+            </a>
+          </span>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center gap-6">
         <div className="flex flex-wrap gap-6 w-[18rem]items-center justify-center sm:w-[23rem]">
           {SPONSORS.map((sponsor) => (
             <Sponsor sponsor={sponsor} key={SPONSORS.indexOf(sponsor)} />
