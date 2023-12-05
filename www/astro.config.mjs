270d41ccc8868b60d209c09fd9bc16c7814ba63f
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-12-05 01:35:39 -0800

    Added og image (#92)
    * Added og image
    
    * Updated site url from https://expostack.dev/ to https://createexpostack.com/
    
    * Updated headSeo
    
    * Added TODO comment for unused api/og code, and replaced property with name in headSeo for twitter tags.

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `https://expostack.dev/`,
+  site: `https://createexpostack.com/`,
   integrations: [react(), tailwind()],
   output: "static",
   adapter: vercel({
