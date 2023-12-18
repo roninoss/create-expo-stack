33e3c8d5209148958b3799ff20c1091721bf815d
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-12-17 21:53:47 -0800

    Updated header to have hamburger menu in mobile mode

--- docs/src/components/Header/Header.tsx
+++ docs/src/components/Header/Header.tsx
@@ -10,10 +10,26 @@ import { EDIT_URL, TWITTER_URL } from "@/consts";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="px-4  sm:container  flex h-14 items-center justify-between">
+      <div className="px-4  sm:container  flex-row flex h-14 items-center justify-between">
         <MainNav />
-        <MobileNav />
-        <div className="flex flex-1 items-center space-x-2 justify-end">
+        <div className="flex flex-row space-x-3">
+          <a href={EDIT_URL} target="_blank" rel="noreferrer" className="flex sm:hidden">
+            <div
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                }),
+                "w-9 px-0",
+              )}
+            >
+              <Icons.gitHub className="h-6 w-6" />
+              <span className="sr-only">GitHub</span>
+            </div>
+          </a>
+          <MobileNav />
+        </div>
+
+        <div className="hidden sm:flex flex-1 items-center space-x-2 justify-end">
           <nav className="flex flex-row items-center space-x-1">
             <a href={EDIT_URL} target="_blank" rel="noreferrer">
               <div
