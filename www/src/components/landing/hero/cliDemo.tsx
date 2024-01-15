f8908ffa9033ac01169380bc7e6c3fc018817353
Nikita Krupin <krupin.nikita0110@gmail.com>
2024-01-15 13:12:47 -0500

    landing community section to take up more available width, cli demo fixed height on desktop

--- www/src/components/landing/hero/cliDemo.tsx
+++ www/src/components/landing/hero/cliDemo.tsx
@@ -14,8 +14,8 @@ export default function CliDemo() {
         window.innerWidth > 768 ? "demo-desktop.cast" : "demo-mobile.cast",
         cliDemoRef.current,
         {
-          rows: window.innerWidth > 1024 ? 37 : null,
-          fit: "width",
+          rows: window.innerWidth > 1024 ? 34 : null,
+          fit: window.innerWidth > 1024 ? "none" : "width",
           theme: "mine",
           controls: false,
           autoPlay: true,
@@ -29,7 +29,7 @@ export default function CliDemo() {
     <div
       id="cli-demo"
       ref={cliDemoRef}
-      className="relative w-[90%] lg:w-[69%] 2xl:w-[60%] h-[55vh] md:h-[46rem] lg:min-h-[50vh] p-4 sm:p-5 lg:p-6 text-white/70 flex flex-col z-[1] rounded-2xl sm:rounded-3xl xl:rounded-[2rem] bg-white/10 overflow-wrap overflow-hidden backdrop-blur-sm opacity-75"
+      className="relative w-[90%] lg:w-[69%] 2xl:w-[60%] h-[55vh] lg:h-[45.5rem] p-4 sm:p-5 lg:p-6 text-white/70 flex flex-col z-[1] rounded-2xl sm:rounded-3xl xl:rounded-[2rem] bg-white/10 overflow-wrap overflow-hidden backdrop-blur-sm opacity-75"
     ></div>
   );
 }
