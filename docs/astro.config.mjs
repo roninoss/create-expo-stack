59a157c5c08305d73038cad1960053b48f900c8b
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-01-26 01:32:49 -0800

    Added rehype and remark

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,10 @@ import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 
 import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeSlug from "rehype-slug";
+import remarkCodeTitles from "remark-code-titles";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,6 +23,37 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noreferrer noopener"],
+          content: {
+            type: "text",
+            value: "↗",
+          },
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            class: "heading-link heading-link--hidden---effects",
+            "data-heading-link": true,
+          },
+          behavior: "wrap",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "dark-plus",
+      wrap: true,
+    },
+  },
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
