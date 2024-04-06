05a23a8c9877b966d0e47bede1413530d5814a7b
Dan Stepanov <dan@danstepanov.com>
2024-04-05 23:58:53 -0700

    [www] add fetch request

--- www/demo/steps/initializeProject.js
+++ www/demo/steps/initializeProject.js
@@ -1,8 +1,33 @@
 import { spinner } from "@clack/prompts";
 
-export async function initializeProject() {
+var myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+myHeaders.append("Content-Type", "application/json");
+
+const fetchURL = "http://git:8000";
+
+export async function initializeProject(cliResults) {
+  const formattedCliResults = JSON.stringify(cliResults, (key, value) =>
+    typeof value === "string" ? value.replace(/'/g, '"') : value,
+  );
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: formattedCliResults,
+    redirect: "follow",
+  };
+
   const s = spinner();
   s.start("Initializing your project...");
-  // CALL ENDPOINT HERE WITH CLI RESULTS
-  s.stop("Project initialized!");
+
+  try {
+    // CALL ENDPOINT HERE WITH CLI RESULTS
+    const response = await fetch(fetchURL, requestOptions);
+    console.log(response.text());
+  } catch (error) {
+    console.log("error", error);
+  } finally {
+    s.stop("Project initialized!");
+  }
 }
