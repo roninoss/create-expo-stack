27ee50d60e467acc12df15d68d34e6fd54af3218
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-10-21 22:36:12 +0100

    fix name

--- www/demo/steps/renderTitle.js
+++ www/demo/steps/renderTitle.js
@@ -1,17 +1,18 @@
 // import figlet from "figlet";
 import gradient from "gradient-string";
 
-import { expoStackTheme, TITLE_TEXT } from "../constants.js";
+import { expoStackTheme } from "../constants.js";
 
+// ronin new
 const asciiTitle = `
 
-  ██████╗ ███╗   ██╗   ███╗   ██╗███████╗██╗    ██╗
-  ██╔══██╗████╗  ██║   ████╗  ██║██╔════╝██║    ██║
-  ██████╔╝██╔██╗ ██║   ██╔██╗ ██║█████╗  ██║ █╗ ██║
-  ██╔══██╗██║╚██╗██║   ██║╚██╗██║██╔══╝  ██║███╗██║
-  ██║  ██║██║ ╚████║██╗██║ ╚████║███████╗╚███╔███╔╝
-  ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝ 
-  
+██████╗  ██████╗ ███╗   ██╗██╗███╗   ██╗
+██╔══██╗██╔═══██╗████╗  ██║██║████╗  ██║
+██████╔╝██║   ██║██╔██╗ ██║██║██╔██╗ ██║
+██╔══██╗██║   ██║██║╚██╗██║██║██║╚██╗██║
+██║  ██║╚██████╔╝██║ ╚████║██║██║ ╚████║
+╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═══╝
+
 `;
 
 export async function renderTitle() {
@@ -23,3 +24,9 @@ export async function renderTitle() {
   // set timeout for 1 second so that the title can render before the CLI runs
   await new Promise((resolve) => setTimeout(resolve, 200));
 }
+
+/**
+            
+
+
+ */
