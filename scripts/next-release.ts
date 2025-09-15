2970d7d5b7aa225b73bdf3242271aaee5dd830a3
Dan Stepanov <dan@danstepanov.com>
2025-09-15 12:43:01 -0700

    feat: add publish next version for rn-new in addition to create-expo-stack

--- scripts/next-release.ts
+++ scripts/next-release.ts
@@ -12,18 +12,31 @@ async function createNextRelease() {
      * Parse the package.json for `create-expo-stack`, and update it with the new
      * "next" (development) version based on the parsed changesets
      */
-    const packageJson = await Bun.file(process.cwd() + '/cli/package.json').json();
-    const newNextVersion = releasePlan.releases.find((release) => release.name === packageJson.name)?.newVersion;
+    const cliPackageJson = await Bun.file(process.cwd() + '/cli/package.json').json();
+    const newNextVersion = releasePlan.releases.find((release) => release.name === cliPackageJson.name)?.newVersion;
 
     if (newNextVersion) {
+      const nextVersionWithHash = `${newNextVersion}-next.${commitHash}`;
+
       /**
-       * Write the new "next" version to `package.json`. It is an ephemeral change
+       * Write the new "next" version to `create-expo-stack` package.json. It is an ephemeral change
        * that lives only for the duration of the CI run, and is not committed
        */
-      packageJson.version = `${newNextVersion}-next.${commitHash}`;
+      cliPackageJson.version = nextVersionWithHash;
 
-      const content = JSON.stringify(packageJson, null, '\t') + '\n';
-      await Bun.write(process.cwd() + '/cli/package.json', content);
+      const cliContent = JSON.stringify(cliPackageJson, null, '\t') + '\n';
+      await Bun.write(process.cwd() + '/cli/package.json', cliContent);
+
+      /**
+       * Also update the rn-new package.json with the same version and update its dependency
+       * on create-expo-stack to match the new next version
+       */
+      const rnNewPackageJson = await Bun.file(process.cwd() + '/packages/rn-new/package.json').json();
+      rnNewPackageJson.version = nextVersionWithHash;
+      rnNewPackageJson.dependencies['create-expo-stack'] = `^${nextVersionWithHash}`;
+
+      const rnNewContent = JSON.stringify(rnNewPackageJson, null, '\t') + '\n';
+      await Bun.write(process.cwd() + '/packages/rn-new/package.json', rnNewContent);
     }
   } catch (error) {
     console.log(error);
