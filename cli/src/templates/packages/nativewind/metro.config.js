43c11fb00534be747a25c1f71c9f458fcff7a2a9
Dan Stepanov <dan@danstepanov.com>
2025-09-24 23:12:52 -0700

    add support for nativewind v5 preview

--- cli/src/templates/packages/nativewind/metro.config.js
+++ cli/src/templates/packages/nativewind/metro.config.js
@@ -1,7 +1,8 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { withNativewind } = require('nativewind/metro');
 
-// eslint-disable-next-line no-undef
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativewind(config);
